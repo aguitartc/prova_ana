95ea56400215b08c3691e521f6e9999029608513
Ana Guitart Carrera <ana.maria.guitart@upc.edu>
2021-10-04 12:22:46 +0200

    modificat el controlador departament proves, ara com home

--- src/Controller/DepartmentController.php
+++ src/Controller/DepartmentController.php
@@ -3,18 +3,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DepartmentController extends AbstractController
 {
     /**
-     * @Route("/department", name="department")
+     * @Route("/hello/{name}", name="homepage")
      */
-    public function index(): Response
+    public function index(string $name = '')
     {
-        return $this->render('department/index.html.twig', [
-            'controller_name' => 'DepartmentController',
-        ]);
+        $greet = '';
+        if ($name)  {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+        return new Response(<<<EOF
+<html>
+<body>
+    $greet
+</body>
+</html>
+    
+
+EOF
+);
     }
 }
