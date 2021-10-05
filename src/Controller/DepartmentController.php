2592a2be1a5a0af26534a817cd19504b637ae913
Ana Maria Guitart <ana.maria.guitart@upc.edu>
2021-10-05 15:47:50 +0200

    llista persones dels departaments i començat amb la paginació, a falta d passar a twig el paginator en comptes del Collection

--- src/Controller/DepartmentController.php
+++ src/Controller/DepartmentController.php
@@ -2,31 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Department;
+use App\Repository\DepartmentRepository;
+use App\Repository\PersonaRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+
 
 class DepartmentController extends AbstractController
 {
     /**
-     * @Route("/hello/{name}", name="homepage")
+     * @Route("/departments", name="homepage")
      */
-    public function index(string $name = '')
+    public function index(Environment $twig, DepartmentRepository $departmentRepository)
     {
-        $greet = '';
-        if ($name)  {
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-        }
-        return new Response(<<<EOF
-<html>
-<body>
-    $greet
-</body>
-</html>
-    
-
-EOF
-);
+        return new Response($twig->render('department/index.html.twig',['departments'=>$departmentRepository->findAll()]));
     }
+
+    /**
+     * @Route("/department/{id}", name="department")
+     */
+    public function show(Environment $twig, Department $department, PersonaRepository $personaRepository)
+        {
+        return new Response($twig->render('department/show.html.twig',
+                                            ['department'=>$department,'persones'=>$personaRepository->findBy(['department'=>$department],['nom'=> 'DESC']),
+                                            ]));
+}
+
 }
--- src/Repository/PersonaRepository.php
+++ src/Repository/PersonaRepository.php
@@ -3,8 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\Persona;
+use App\Entity\Department;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 /**
  * @method Persona|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,11 +16,26 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class PersonaRepository extends ServiceEntityRepository
 {
+    public const PAGINATOR_PER_PAGE = 2;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Persona::class);
     }
 
+    public function getPersonaPaginator(Department $department, int $offset):
+    Paginator
+    {
+        $query = $this->createQueryBuilder('c')
+            ->andWhere('c.department = ;department')
+            ->setParameter('department', $department)
+            ->orderBy('c.nom', 'ASC')
+            ->setMaxResults($offset)
+            ->getQuery()
+        ;
+
+        return new Paginator($query);
+    }
     // /**
     //  * @return Persona[] Returns an array of Persona objects
     //  */
