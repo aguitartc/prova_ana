4a8d63f222e41ddd2eac915b7a872667dbe3ed10
Ana Maria Guitart <ana.maria.guitart@upc.edu>
2021-10-05 16:27:40 +0200

    refactoring: passa twig a atribut i deixa d passar-se x paràmetre a les funcs

--- src/Controller/DepartmentController.php
+++ src/Controller/DepartmentController.php
@@ -16,23 +16,29 @@ use Twig\Environment;
 
 class DepartmentController extends AbstractController
 {
+    private $twig;
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     /**
      * @Route("/departments", name="homepage")
      */
-    public function index(Environment $twig, DepartmentRepository $departmentRepository)
+    public function index(DepartmentRepository $departmentRepository)
     {
-        return new Response($twig->render('department/index.html.twig',['departments'=>$departmentRepository->findAll()]));
+        return new Response($this->twig->render('department/index.html.twig',['departments'=>$departmentRepository->findAll()]));
     }
 
     /**
      * @Route("/department/{id}", name="department")
      */
-    public function show(Request $request, Environment $twig, Department $department, PersonaRepository $personaRepository)
+    public function show(Request $request, Department $department, PersonaRepository $personaRepository)
         {
             $offset     = max(0,$request->query->getInt('offset',0));
             $paginator  =  $personaRepository->getPersonaPaginator($department,$offset);
 
-        return new Response($twig->render('department/show.html.twig',
+        return new Response($this->twig->render('department/show.html.twig',
                                             ['department'=>$department,
                                                 'persones' => $paginator,
                                                 'previous' => $offset - PersonaRepository::PAGINATOR_PER_PAGE,
